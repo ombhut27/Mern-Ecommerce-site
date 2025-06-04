c8754efaa39d0b5552f8a65dd370517e5a99bb27
ombhut27 <122210267+ombhut27@users.noreply.github.com>
2025-06-04 13:41:14 +0530

    Update userAuth.js

--- backend/middleware/userAuth.js
+++ backend/middleware/userAuth.js
@@ -1,7 +1,5 @@
-import jwt from "jsonwebtoken";
-
-  const userAuth = async (req, res, next) => {
-    const { token } = req.cookies;
+const userAuth = async (req, res, next) => {
+    const token = req.cookies.token || req.headers.token;
 
     if (!token) {
         return res.json({ success: false, message: 'Not Authorized. Login Again' });
@@ -11,16 +9,15 @@ import jwt from "jsonwebtoken";
         const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
         if (tokenDecode.id) {
             req.body.userId = tokenDecode.id;
+            next();
         } else {
             return res.json({ success: false, message: 'Not Authorized. Login Again' });
         }
-        next(); 
     } catch (error) {
         return res.json({ success: false, message: error.message });
     }
 };
 
-export default userAuth
 
 
 
